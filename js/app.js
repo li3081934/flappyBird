4858dd31bd06ae7b74fc37d08422e7b9f4229939
li3081934 <jie3081934@126.com>
2017-09-11 17:38:11 +0800

    create files

---
+++ js/app.js
@@ -0,0 +1,158 @@
+class Draw{
+    constructor(context,position,img){
+        this.position=position;
+        this.context=context;
+        this.img=img;
+        this.size={
+            w:img.width,
+            h:img.height
+        }
+    }
+    draw(){
+        let {x,y}=this.position;
+        this.context.drawImage(this.img,x,y)
+    }
+    hitCheck(o2,o1=this){
+        let {x:x2,y:y2}=o2.position,{w:w2,h:h2}=o2.size;
+        let {x:x1,y:y1}=o1.position,{w:w1,h:h1}=o1.size;
+        let o1center={
+            x:w1/2+x1,
+            y:h1/2+y1
+        };
+        let o2center={
+            x:w2/2+x2,
+            y:h2/2+y2
+        };
+
+        if(Math.abs(o1center.x-o2center.x)<=w1/2+w2/2){
+            if(Math.abs(o1center.y-o2center.y)<=h1/2+h2/2){
+
+                let crossX=x1+w1-x2>w1?x2+w2-x1:x1+w1-x2;
+                let crossY=y1+h1-y2>h1?y2+h2-y1:y1+h1-y2;
+                if(crossX>crossY){
+                    if(y2>y1){
+                        return 'top'
+                    }else{
+                        return 'bottom'
+                    }
+
+                }else{
+                    if(x2>x1){
+                        return 'right'
+                    }else{
+                        return 'left'
+                    }
+
+                }
+            }
+        }
+
+    }
+}
+function createImgObj(url){
+    let img=new Image();
+    img.src=url;
+    return img;
+}
+class Layout{
+    constructor(container,index,size){
+        debugger
+        this.size=size
+        this.wrap=document.createElement('div');
+        this.canvas=document.createElement('canvas');
+        this.canvas.width=size.w;
+        this.canvas.height=size.h;
+        this.ctx=this.canvas.getContext('2d');
+        this.wrap.className='layout_'+index;
+        this.wrap.appendChild(this.canvas)
+        container.appendChild(this.wrap)
+    }
+    draw(img=this.img,position=this.position){
+        let {x,y}=position;
+        this.img=img;
+        this.position=position
+        this.ctx.drawImage(img,x,y)
+    }
+    clear(){
+        this.ctx.clearRect(0,0,this.size.w,this.size.h)
+    }
+
+
+}
+class LayoutGround extends Layout{
+    constructor(container,index,size){
+        super(container,index,size)
+    }
+    scroll(){
+        this.timer=setInterval(()=>{
+            this.clear();
+            let x=0;
+            //debugger
+            x=this.position.x;
+            x-=3;
+            x%=24;
+
+            this.position.x=x
+            this.draw()
+        },1000/30)
+    }
+}
+class LayoutBird extends Layout{
+    constructor(container,index,size){
+        super(container,index,size)
+        this.cutPosition={
+            x:0,
+            y:0
+        }
+    }
+    draw(img=this.img,position=this.position){
+        let {x,y}=position,{x:cx,y:cy}=this.cutPosition;
+        this.img=img;
+        this.position=position
+        this.ctx.drawImage(img,cx,cy,36,26,x,y,36,26)
+    }
+    fly(){
+        this.timer=setInterval(()=>{
+            this.clear();
+            let x=0;
+            debugger
+            x=this.cutPosition.x;
+            x+=36;
+            x%=108;
+
+            this.cutPosition.x=x
+            this.draw()
+        },1000/12)
+    }
+}
+class Scene{
+    constructor(container){
+        this.bgImg=createImgObj('./images/bg.png')
+        this.container=container
+        this.bgImg.onload=()=>{
+            let bgLayout=new Layout(this.container,0,{w:288,h:384})
+            this.bgLayout=bgLayout
+            bgLayout.draw(this.bgImg,{x:0,y:0})
+        }
+
+
+        this.groundImg=createImgObj('./images/ground.png')
+        this.groundImg.onload=()=>{
+            let groundLayout=new LayoutGround(this.container,1,{w:288,h:384})
+            this.groundLayout=groundLayout
+            groundLayout.draw(this.groundImg,{x:0,y:362})
+        }
+
+
+        this.birdImg=createImgObj('./images/bird.png')
+        this.birdImg.onload=()=>{
+            let birdLayout=new LayoutBird(this.container,2,{w:288,h:384})
+            this.birdLayout=birdLayout
+            birdLayout.draw(this.birdImg,{x:140,y:150})
+        }
+    }
+    start(){
+        this.groundLayout.scroll()
+        this.birdLayout.fly()
+    }
+}
